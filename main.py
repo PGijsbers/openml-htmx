9c24afcaecd6c652fc8cd7d8edc1059fdb9600e2
PGijsbers <p.gijsbers@tue.nl>
2024-01-18 22:49:08 +0100

    Add dataset card template and render markdown description

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import markdown
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from string import Template
 
-from openml import get_datasets
+from openml import get_datasets, get_dataset
 
 app = FastAPI()
 
 
 @app.get("/blue", response_class=HTMLResponse)
 def get_blue():
-    dataset = get_datasets()[-1]["name"]
-    return f"<div style=\"background:blue;color:white;\">{dataset}</div>"
+    with open("html/dataset-card.html", "r") as fh:
+        template = Template(fh.read())
+    last_id = get_datasets()[-1]["did"]
+    dataset = get_dataset(last_id)
+    openml_url = f"https://openml.org/d/{last_id}"
+    dataset["description"] = markdown.markdown(dataset["description"])
+    return template.substitute(**dataset, openml_url=openml_url)
 
 @app.get("/", response_class=HTMLResponse)
 def get_home():
--- openml.py
+++ openml.py
@@ -8,3 +8,9 @@ def get_datasets():
         json={"pagination": {"offset": 10, "limit": 5}}
     )
     return r.json()
+
+def get_dataset(id_: int):
+    r = httpx.get(
+        f"{OPENML_URL}/datasets/{id_}"
+    )
+    return r.json()
