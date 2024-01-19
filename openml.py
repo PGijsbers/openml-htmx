1f0d0f5299878d8597850ce803e81308a76f7691
PGijsbers <p.gijsbers@tue.nl>
2024-01-19 19:36:02 +0100

    Infite scrolling of static endpoint

--- openml.py
+++ openml.py
@@ -3,11 +3,11 @@ import httpx
 OPENML_URL = "https://test.openml.org/py"
 
 def get_datasets():
-    r = httpx.post(
+    response = httpx.post(
         f"{OPENML_URL}/datasets/list",
         json={"pagination": {"offset": 10, "limit": 5}}
     )
-    return r.json()
+    return response.json()
 
 def get_dataset(id_: int):
     r = httpx.get(
